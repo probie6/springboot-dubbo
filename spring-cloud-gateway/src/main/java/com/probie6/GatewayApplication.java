74f7429bd8994b99d6aa9724387475214fa07218
王飞 <731746237@qq.com>
2020-01-16 14:29:19 +0800

    根目录build.gralde调整

---
+++ spring-cloud-gateway/src/main/java/com/probie6/GatewayApplication.java
@@ -0,0 +1,14 @@
+package com.probie6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * create by 王飞 on 2020-01-16
+ */
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
